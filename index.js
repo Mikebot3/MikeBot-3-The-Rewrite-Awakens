0d886f60a51e0b9780a0b257ea60ebec8736c9be
wombat24455 <wombathodge@gmail.com>
2021-12-22 16:44:16 +0000

    changes
    idk man just rewrote all the goddamn code ig?

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* Required Modules */
-import Discord, { Intents } from 'discord.js';
+const fs = require('fs');
+import Discord, { Collection, Intents } from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,14 +10,21 @@ const client = new Discord.Client({
         Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_MESSAGES
     ]
-
 })
 /* Required Modules */
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command)
+}
 
 /* Event Handlers */
 client.on('ready', () => {
     console.log(`Hello World, I am ${client.user.tag}`);
-
+    /*
     const Mikebot = process.env.Mikebot;
     const guild = client.guilds.cache.get(Mikebot);
     let commands;
@@ -34,25 +42,23 @@ client.on('ready', () => {
         description: "test"
         }
     )
+    */
 })
 
 client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand()) {
-        interaction.reply({ content: 'not a command bruh', ephemeral: true, })
-        return
-    }
+    if (!interaction.isCommand()) return;
 
-    const { commandName, options } = interaction
+    const command = client.commands.get(interaction.commandName)
 
-    switch (commandName) {
-        case "ping":
-            interaction.reply({ content: 'pong', ephemeral: true, })
-            break;
-        case "test":
-            interaction.reply({ content: "test", ephemeral: false })
-            break;
+    if (!command) return;
+
+    try {
+        command.execute(interaction);
+    } catch (error) {
+        console.error(`WTF LOL:\n${error}`);
+        interaction.reply({ content: 'oi bruv somefing went rong innit', ephemeral: true, })
     }
-})
+});
 /* Event Handlers */
 
 client.login(process.env.TOKEN);
