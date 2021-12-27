69f3e67730d40bfffb6ec5b227f17f03e14f43ae
Ci Astra <68437385+cixastra@users.noreply.github.com>
2021-12-27 13:39:17 -0600

    i think thatll work?

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const download = require('download-git-repo');
 download('Mikebot3/Mikebot-3-The-Rewrite-Awakens', ".../", function (err) {
     console.log(err ? 'Error' : 'Success')
   });
+const bot = require('.../bot.js');
+bot();
